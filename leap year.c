2334effd862384e71e9fbf19475145ec006dcd20
soundaryavijay <soundaryavijay8525952025@gmail.com>
2016-12-26 11:27:22 +0530

    Add files via upload

---
+++ leap year.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int year;
+printf("Enter the year");
+scanf("%d",&year);
+if((year%4)==0)
+printf("It is a leap year");
+else
+printf("It is not a leap year");
+
+}
