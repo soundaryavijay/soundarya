419899eb11df810c59109f672ebde384a8deac84
soundaryavijay <soundaryavijay8525952025@gmail.com>
2016-12-26 14:32:11 +0530

    Add files via upload

---
+++ palindrome.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char str1[20];
+    int i, len;
+    int flag = 0;
+
+    printf("Enter a string:");
+    scanf("%s", str1);
+
+    len = strlen(str1);
+
+    for(i=0;i < len ;i++){
+        if(str1[i] != str1[len-i-1]){
+            flag = 1;
+            break;
+	   }
+	}
+
+    if (flag) {
+        printf("%s is not a palindrome", str1);
+    }
+    else {
+        printf("%s is a palindrome", str1);
+    }
+    return 0;
+}
