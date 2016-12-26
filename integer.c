ead20c66655d80f8f9173e9873b61d650df87274
soundaryavijay <soundaryavijay8525952025@gmail.com>
2016-12-26 09:20:09 +0530

    Add files via upload

---
+++ integer.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("Enter the value :");
+    scanf("%d",&a);
+    if(a>0)
+    printf("positive number\n");
+    else if(a<0)
+        printf("negative number");
+    else
+    printf("zero");
+
+
+
+    }
