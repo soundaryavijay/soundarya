807a34dfb80e0695663c8159ed83b7829df0bc88
soundaryavijay <soundaryavijay8525952025@gmail.com>
2016-12-26 10:40:59 +0530

    Add files via upload

---
+++ alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char ch;
+scanf("%c",&ch);
+if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+printf("It is an alphabet\n");
+else
+printf("It is not an alphabet");
+
+
+
+}
