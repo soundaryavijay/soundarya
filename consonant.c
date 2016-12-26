eacea28b430a495cfb8a85143c63dab998d5ae71
soundaryavijay <soundaryavijay8525952025@gmail.com>
2016-12-26 10:21:52 +0530

    Add files via upload

---
+++ consonant.c
@@ -0,0 +1,22 @@
+
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+
+char s;
+printf("Enter the character:\n");
+scanf("%c",&s);
+if(s=='a'||s=='e'||s=='i'||s=='o'||s=='u'||s=='A'||s=='E'||s=='I'||s=='o'||s=='u')
+{
+printf("It is a vowel");
+}
+else{
+printf("It is a consonant");
+}
+
+
+
+
+}
+
